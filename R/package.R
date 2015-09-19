5ab4c183d97c0ee765d5f8805d3db18211d7e820
jennybc <jenny@stat.ubc.ca>
2015-09-18 23:35:22 -0700

    add an argument for user-specified header fields

--- R/package.R
+++ R/package.R
@@ -13,24 +13,25 @@ api_url <- "https://api.github.com"
 
 ## Headers to send with each API request
 
-send_headers <- c("accept" = "application/vnd.github.v3+json",
-                  "user-agent" = "https://github.com/gaborcsardi/whoami")
+send_headers <- c("Accept" = "application/vnd.github.v3+json",
+                  "User-Agent" = "https://github.com/gaborcsardi/whoami")
 
 #' Query the GitHub API
 #'
 #' This is an extremely minimal client. You need to know the API
 #' to be able to use this client. All this function does is
 #' \itemize{
-#'   \item Tries to substitute each listed parameter into
+#'   \item Try to substitute each listed parameter into
 #'     \code{endpoint}, using the \code{:parameter} notation.
-#'   \item If a GET request (the default), then adds
+#'   \item If a GET request (the default), then add
 #'     all other listed parameters as query parameters.
-#'   \item If not a GET request, then sends the other parameters
+#'   \item If not a GET request, then send the other parameters
 #'     in the request body, as JSON.
-#'   \item Converts the response to an R list using
-#'     \code{jsonline::fromJSON}.
+#'   \item Convert the response to an R list using
+#'     \code{jsonlite::fromJSON}.
 #' }
 #'
+
 #' @param endpoint GitHub API endpoint. See examples below.
 #' @param ... Additional parameters
 #' @param .token Authentication token.
@@ -42,6 +43,16 @@ send_headers <- c("accept" = "application/vnd.github.v3+json",
 #'   Note, that if you request many records, then multiple GitHub
 #'   API calls are used to get them, and this can take a potentially
 #'   long time.
+#' @param .send_headers Named character vector of header field values
+#'   (excepting \code{Authorization}, which is handled via
+#'   \code{.token}). This can be used to override or augment the
+#'   defaults, which are as follows: the \code{Accept} field defaults
+#'   to \code{"application/vnd.github.v3+json"} and the
+#'   \code{User-Agent} field defaults to
+#'   \code{"https://github.com/gaborcsardi/whoami"}. This can be used
+#'   to, e.g., provide a custom media type, in order to access a
+#'   preview feature of the API.
+#'
 #' @return Answer from the API.
 #'
 #' @importFrom httr content add_headers headers
@@ -65,10 +76,15 @@ send_headers <- c("accept" = "application/vnd.github.v3+json",
 #' ## Automatic pagination
 #' users <- gh("/users", .limit = 50)
 #' length(users)
+#'
+#' ## Access developer preview of Licenses API (in preview on 2015-09-20)
+#' gh("/licenses") # error code 415
+#' gh("/licenses",
+#'    .send_headers = c("Accept" = "application/vnd.github.drax-preview+json"))
 #' }
 
 gh <- function(endpoint, ..., .token = Sys.getenv('GITHUB_TOKEN'),
-               .limit = NULL) {
+               .limit = NULL, .send_headers = NULL) {
 
   params <- list(...)
 
@@ -78,10 +94,11 @@ gh <- function(endpoint, ..., .token = Sys.getenv('GITHUB_TOKEN'),
   params <- parsed$params
 
   auth <- get_auth(.token)
+  headers <- get_headers(.send_headers)
 
   url <- paste0(api_url, endpoint)
 
-  res <- gh_url(method, url, auth, params)
+  res <- gh_url(method, url, auth, headers, params)
 
   while (! is.null(.limit) && length(res) < .limit && gh_has_next(res)) {
     res2 <- gh_next(res, .token = .token)
@@ -100,8 +117,19 @@ get_auth <- function(token) {
   auth
 }
 
+get_headers <- function(headers = NULL) {
 
-gh_url <- function(method, url, auth, params) {
+  if (is.null(headers))
+    send_headers
+  else
+    c(headers,
+      send_headers[ ! tolower(names(send_headers)) %in%
+                      tolower(names(headers))])
+
+}
+
+
+gh_url <- function(method, url, auth, headers, params) {
 
   method_fun <- list("GET" = GET, "POST" = POST, "PATCH" = PATCH,
                      "PUT" = PUT, "DELETE" = DELETE)[[method]]
@@ -114,18 +142,18 @@ gh_url <- function(method, url, auth, params) {
   if (method == "GET" && length(params) > 0) {
     response <- GET(
       url = url,
-      add_headers(.headers = c(send_headers, auth)),
+      add_headers(.headers = c(headers, auth)),
       query = params
     )
   } else if (method == "GET") {
     response <- GET(
       url = url,
-      add_headers(.headers = c(send_headers, auth))
+      add_headers(.headers = c(headers, auth))
     )
   } else {
     response <- method_fun(
       url = url,
-      add_headers(.headers = c(send_headers, auth)),
+      add_headers(.headers = c(headers, auth)),
       body = toJSON(params, auto_unbox = TRUE)
     )
   }

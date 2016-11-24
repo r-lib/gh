7fa5ff86252a83c7f95dce3118323866c05349c0
Gábor Csárdi <csardi.gabor@gmail.com>
2016-11-24 00:48:05 +0000

    Better test recording / replaying setup

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,9 +1,19 @@
 
+## If there is a GITHUB TOKEN file, then
+## * we are replaying previously recorded responses
+## * we are performing and recording unknown requests
+##
+## If there is no GITHUB TOKEN file, then
+## * we are replaying previously recorded responses
+## * we are performing unseen requests, but this should not happen.
+
 if (file.exists("github-token.txt")) {
   Sys.setenv(GITHUB_TOKEN = readLines("github-token.txt", n = 1))
+  Sys.setenv(DEBUGME = "httrmock")
+  httrmock::start_recording()
+  httrmock::start_replaying()
+
+} else {
+  httrmock::stop_recording()
+  httrmock::start_replaying()
 }
-
-## Sys.setenv(DEBUGME = "httrmock")
-
-httrmock::start_replaying()
-httrmock::stop_recording()

9b425c137dbf057a7bc239c41ea03eb360cf1924
Gábor Csárdi <csardi.gabor@gmail.com>
2017-07-12 13:38:04 +0100

    Fix rate limit issue on Travis

--- tests/testthat/test-mock-error.R
+++ tests/testthat/test-mock-error.R
@@ -16,7 +16,10 @@ test_that("can catch a given status directly", {
   skip_if_offline()
   skip_on_cran()
   
-  e <- tryCatch(gh("/missing", .token = ""), "http_error_404" = identity)
+  e <- tryCatch(
+    gh("/missing", .token = tt()),
+    "http_error_404" = identity
+  )
 
   expect_s3_class(e, "github_error")
   expect_s3_class(e, "http_error_404")

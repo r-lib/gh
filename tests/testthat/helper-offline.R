f74140afad49ae10ee2efef7f3c035c7f4cdf0a6
Jennifer (Jenny) Bryan <jenny.f.bryan@gmail.com>
2017-07-25 13:27:52 -0700

    Common offline status for all tests (#64)

--- tests/testthat/helper-offline.R
+++ tests/testthat/helper-offline.R
@@ -1,11 +1,13 @@
 
-skip_if_offline <- function() {
-  ping_res <- tryCatch(pingr::ping_port("github.com", count = 1, timeout = 0.2),
-                       error = function(e) NA)
-  if (is.na(ping_res)) {
-    skip("Offline")
-  }
+offline <- function() {
+  ping_res <- tryCatch(
+    pingr::ping_port("github.com", count = 1, timeout = 0.2),
+    error = function(e) NA
+  )
+  is.na(ping_res)
 }
+OFFLINE <- offline()
+skip_if_offline <- function() if (OFFLINE) skip("Offline")
 
 skip_if_no_token <- function() {
   if (is.na(Sys.getenv("GH_TESING", NA_character_))) {

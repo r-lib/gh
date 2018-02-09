2b4c42739d4b50751ed29908d2ca64f91383ec08
James J Balamuta <coatless@users.noreply.github.com>
2018-02-08 23:32:10 -0600

    Fixed environment variable `GH_TESING` => `GH_TESTING`
    c.f. https://github.com/r-lib/gh/blob/master/tests/testthat/helper.R#L12

--- tests/testthat/helper-offline.R
+++ tests/testthat/helper-offline.R
@@ -13,7 +13,7 @@ skip_if_offline <- (function() {
 })()
 
 skip_if_no_token <- function() {
-  if (is.na(Sys.getenv("GH_TESING", NA_character_))) {
+  if (is.na(Sys.getenv("GH_TESTING", NA_character_))) {
     skip("No GitHub token")
   }
 }

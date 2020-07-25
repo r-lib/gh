72158a907e6d3f6de981ebc86986f84bf91ae69a
Jenny Bryan <jenny.f.bryan@gmail.com>
2020-07-24 21:35:06 -0700

    Add missing skips

--- tests/testthat/test-mock-error.R
+++ tests/testthat/test-mock-error.R
@@ -4,7 +4,8 @@ test_that("errors return a github_error object", {
 
   skip_if_offline()
   skip_on_cran()
-  
+  skip_if_no_token()
+
   e <- tryCatch(gh("/missing", .token = tt()), error = identity)
 
   expect_s3_class(e, "github_error")
@@ -15,7 +16,8 @@ test_that("can catch a given status directly", {
 
   skip_if_offline()
   skip_on_cran()
-  
+  skip_if_no_token()
+
   e <- tryCatch(
     gh("/missing", .token = tt()),
     "http_error_404" = identity

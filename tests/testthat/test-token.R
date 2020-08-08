3a8b64528738adcb703f614f3db75370e5925562
Jenny Bryan <jenny.f.bryan@gmail.com>
2020-08-07 21:54:46 -0700

    Introduce gh_pat class
    Closes #125

--- tests/testthat/test-token.R
+++ tests/testthat/test-token.R
@@ -1,55 +1,78 @@
+test_that("URL specific token is used", {
+  good  <- gh_pat(strrep("a", 40))
+  good2 <- gh_pat(strrep("b", 40))
+  bad   <- gh_pat(strrep("0", 40))
+  bad2  <- gh_pat(strrep("1", 40))
 
-test_that("api specific token is used", {
   env <- c(
     GH_KEYRING = "false",
     GITHUB_API_URL = "https://github.acme.com",
-    GITHUB_PAT_GITHUB_ACME_COM = "good",
-    GITHUB_PAT_GITHUB_ACME2_COM = "good2",
-    GITHUB_PAT = "bad",
-    GITHUB_TOKEN = "bad2"
+    GITHUB_PAT_GITHUB_ACME_COM = good,
+    GITHUB_PAT_GITHUB_ACME2_COM = good2,
+    GITHUB_PAT = bad,
+    GITHUB_TOKEN = bad2
   )
   withr::with_envvar(env, {
-    expect_equal(gh_token(), "good")
-    expect_equal(gh_token("https://github.acme2.com"), "good2")
+    expect_equal(gh_token(), good)
+    expect_equal(gh_token("https://github.acme2.com"), good2)
   })
 
-  env2 <- c(
+  env <- c(
     GH_KEYRING = "false",
     GITHUB_API_URL = NA,
-    GITHUB_PAT_API_GITHUB_COM = "good",
-    GITHUB_PAT = "bad",
-    GITHUB_TOKEN = "bad2"
+    GITHUB_PAT_API_GITHUB_COM = good,
+    GITHUB_PAT = bad,
+    GITHUB_TOKEN = bad2
   )
-  withr::with_envvar(env2, {
-    expect_equal(gh_token(), "good")
-    expect_equal(gh_token("https://api.github.com"), "good")
+  withr::with_envvar(env, {
+    expect_equal(gh_token(), good)
+    expect_equal(gh_token("https://api.github.com"), good)
   })
 })
 
-test_that("fall back to GITHUB_PAT", {
+test_that("fall back to GITHUB_PAT, then GITHUB_TOKEN", {
+  pat   <- gh_pat(strrep("a", 40))
+  token <- gh_pat(strrep("0", 40))
+
   env <- c(
     GH_KEYRING = "false",
     GITHUB_API_URL = NA,
     GITHUB_PAT_API_GITHUB_COM = NA,
-    GITHUB_PAT = "pat",
-    GITHUB_TOKEN = "token"
+    GITHUB_PAT = pat,
+    GITHUB_TOKEN = token
   )
   withr::with_envvar(env, {
-    expect_equal(gh_token(), "pat")
-    expect_equal(gh_token("https://api.github.com"), "pat")
+    expect_equal(gh_token(), pat)
+    expect_equal(gh_token("https://api.github.com"), pat)
   })
-})
 
-test_that("fall back to GITHUB_TOKEN", {
   env <- c(
     GH_KEYRING = "false",
     GITHUB_API_URL = NA,
     GITHUB_PAT_API_GITHUB_COM = NA,
     GITHUB_PAT = NA,
-    GITHUB_TOKEN = "token"
+    GITHUB_TOKEN = token
   )
   withr::with_envvar(env, {
-    expect_equal(gh_token(), "token")
-    expect_equal(gh_token("https://api.github.com"), "token")
+    expect_equal(gh_token(), token)
+    expect_equal(gh_token("https://api.github.com"), token)
   })
+})
+
+# gh_pat class
+test_that("validate_gh_pat() rejects bad characters, wrong # of characters", {
+  expect_error(gh_pat(strrep("a", 40)), NA)
+  expect_error(gh_pat(strrep("g", 40)), "40 hexadecimal digits")
+  expect_error(gh_pat("aa"), "40 hexadecimal digits")
+})
+
+test_that("format.gh_pat() and str.gh_pat() hide the middle stuff", {
+  pat <- paste0(strrep("a", 10), strrep("4", 20), strrep("F", 10))
+  expect_match(format(gh_pat(pat)), "[a-zA-Z]+")
+  expect_output(str(gh_pat(pat)), "[a-zA-Z]+")
+})
+
+test_that("str.gh_pat() indicates it's a `gh_pat`", {
+  pat <- paste0(strrep("a", 10), strrep("4", 20), strrep("F", 10))
+  expect_output(str(gh_pat(pat)), "gh_pat")
 })

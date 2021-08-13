561fca18162a239a3dba31629c4a51ce1921b1c4
Sébastien Rochette <statnmap@users.noreply.github.com>
2021-08-13 13:55:13 +0200

    New example of graphql query
    Proposition of use for graphql query to retrieve the graphql rate limit, different from the REST API one as explained here: https://docs.github.com/en/graphql/overview/resource-limitations

--- R/gh_gql.R
+++ R/gh_gql.R
@@ -13,6 +13,20 @@
 #' @seealso [gh()] for the GitHub v3 API.
 #' @examplesIf FALSE
 #' gh_gql("query { viewer { login }}")
+#' Get rate limit
+#' ratelimit_query <- "query {
+#'   viewer {
+#'     login
+#'   }
+#'   rateLimit {
+#'     limit
+#'     cost
+#'     remaining
+#'     resetAt
+#'   }
+#' }"
+#' 
+#' gh::gh_gql(ratelimit_query)
 
 gh_gql <- function(query, ...) {
 

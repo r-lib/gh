8b1ad20742140e58490c5d3772bd00e154219170
Tan Ho <tan@tanho.ca>
2024-08-15 12:07:17 -0700

    refactor parameter handling to make testable without mocking

--- R/utils.R
+++ R/utils.R
@@ -76,6 +76,12 @@ drop_named_nulls <- function(x) {
   cleanse_names(x[!named | !null])
 }
 
+.parse_params <- function(..., .params = list()) {
+  params <- c(list(...), .params)
+  params <- drop_named_nulls(params)
+  return(params)
+}
+
 check_named_nas <- function(x) {
   if (has_no_names(x)) {
     return(x)

5924bc2fa17328f55ec57ad00f593a30b5105666
Gábor Csárdi <csardi.gabor@gmail.com>
2026-05-27 14:55:59 +0200

    Fix snapshot test on windows

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -66,5 +66,6 @@ transform_tempdir <- function(x) {
   x <- sub("\\R\\", "/R/", x, fixed = TRUE)
   x <- sub("[\\\\/]file[a-zA-Z0-9]+", "/<tempfile>", x)
   x <- sub("[A-Z]:.*Rtmp[a-zA-Z0-9]+[\\\\/]", "<tempdir>/", x)
+  x <- gsub("'[A-Z]:/'", "'/'", x)
   x
 }
